9a50f192531032054477ccbbfde870fad084106f
abbythompson2 <129601900+abbythompson2@users.noreply.github.com>
2025-03-20 12:56:54 -0400

    Update home.php

--- home.php
+++ home.php
@@ -7,22 +7,15 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Refined Recipes</title>
-    <link rel="stylesheet" href="home.css">
 </head>
 <header>
     <div class="top">
-        <h1>Refined Recipes</h1>
-        <h4>Explore a variety of recipes and add your own!</h4>
+        <h1>Travel Manager</h1>
+        <h4>Manage all the details of your trip!</h4>
     </div>
     <nav>
-        <button class="button" onclick="location.href='Homepage.php'">Home</button>
-        <button class="button" onclick="location.href='db.php'">Add Recipe</button>
-        <button class="button" onclick="location.href='RecipeSearch.PHP'">Search for Recipe</button>
-        <button class="button" onclick="location.href='RecipeSearch.PHP'">All Recipes</button>
-        <button class="button" onclick="location.href='aboutUs.PHP'">About Us</button>
-        <button class="button" onclick="location.href='review.PHP'">Reviews</button>
         <?php
-        if (isset($_SESSION['user_id'])) {
+        if (isset($_SESSION['ID'])) {
             echo '<button class="button" onclick="location.href=\'logout.PHP\'">Logout</button>';
         }else{
             echo '<button class="button" onclick="location.href=\'login.PHP\'">Login</button>';
@@ -31,7 +24,6 @@ session_start();
     </nav>   
 </header>
 <body>
-    <img src="recipe.jpg" alt="Logo for website">
 </body>
 <footer>
 
