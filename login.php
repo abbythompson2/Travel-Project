f736fa99fafa00ff8f8c60ba0651f13b064e8ccc
abbythompson2 <129601900+abbythompson2@users.noreply.github.com>
2025-03-26 18:39:31 -0400

    Update login.php

--- login.php
+++ login.php
@@ -12,14 +12,7 @@
         <h4>Start planning your today!</h4></h4>
     </div>
     <nav>
-        <button class="button" onclick="location.href='Homepage.php'">Home</button>
-        <?php
-        if (isset($_SESSION['user_id'])) {
-            echo '<button class="button" onclick="location.href=\'logout.PHP\'">Logout</button>';
-        }else{
-            echo '<button class="button" onclick="location.href=\'login.PHP\'">Login</button>';
-        }
-        ?>   
+        <button class="button" onclick="location.href='home.php'">Home</button> 
     </nav>   
 </header>
 <body>
@@ -28,13 +21,14 @@ session_start();
 
 $servername = "localhost";
 $username = "root";
-$password = "root";
-$dbname = "project";
+$password = "";
+$dbname = "trip-project";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+} 
+catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 
@@ -42,13 +36,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch();
 
-    if ($user && password_verify($password, $user['password_hash'])) {
+    if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
-        header("Location: Homepage.php");
+        header("Location: home.php");
         exit(); // Ensure that no further code runs after redirect
     } else {
         header("Location: register.php?error=1");
