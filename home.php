25375ba6fe4030d9335eb286c6e7406f7f6f2ed1
abbythompson2 <129601900+abbythompson2@users.noreply.github.com>
2025-03-27 09:20:58 -0400

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Refined Recipes</title>
+    <title>Travel Manager</title>
 </head>
 <header>
     <div class="top">
@@ -15,9 +15,39 @@ session_start();
     </div>
     <nav>
         <?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "trip-project"; 
+
+        try {
+            $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } 
+
+
+        catch (PDOException $e) {
+            error_log("Database Error: " . $e->getMessage());
+            die("An error occurred. Please try again later.");
+        }
+
+
+
+
         if (isset($_SESSION['user_id'])) {
             echo '<button class="button" onclick="location.href=\'logout.PHP\'">Logout</button>';
             echo '<button class="button" onclick="location.href=\'createTrip.PHP\'">Create Trip</button>';
+
+
+            $stmt = $pdo->prepare("SELECT * FROM trip WHERE userID = ?");
+            $stmt->execute([$_SESSION['user_id']]);
+            $trips = $stmt->fetchAll();
+
+            if (count($trips) >= 1) { 
+                echo "You have a trip!";
+            }
+
         }
         
         else{
